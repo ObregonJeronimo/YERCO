cc674996acef5ffaefce46cd307a8a446ad280fc
Obregón Jerónimo <112417250+ObregonJeronimo@users.noreply.github.com>
2026-03-04 18:40:36 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -8,12 +8,12 @@
  */
 
 const firebaseConfig = {
-    apiKey: "TU_API_KEY",
-    authDomain: "TU_PROYECTO.firebaseapp.com",
-    projectId: "TU_PROJECT_ID",
-    storageBucket: "TU_PROYECTO.firebasestorage.app",
-    messagingSenderId: "TU_SENDER_ID",
-    appId: "TU_APP_ID"
+    apiKey: "AIzaSyCYTYtrsLipyXeWbOUR7sUm3NPLA0mHvgs",
+    authDomain: "yerco-bb620.firebaseapp.com",
+    projectId: "yerco-bb620",
+    storageBucket: "yerco-bb620.firebasestorage.app",
+    messagingSenderId: "1035002416128",
+    appId: "1:1035002416128:web:28ca04eb30b4dce8271ce5"
 };
 
 // Inicializar Firebase
