8091123738a026c2d364063fcdf3341c959973d1
Obregón Jerónimo <112417250+ObregonJeronimo@users.noreply.github.com>
2026-03-04 21:31:41 -0300

    firebase-config.js - limpiar, Storage ya configurado en storageBucket

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,6 @@
 /**
- * ============================================
  * YERCO DIETÉTICA - CONFIGURACIÓN FIREBASE
- * ============================================
- * 
- * INSTRUCCIONES: Reemplazá los valores de abajo
- * con los de tu proyecto de Firebase.
  */
-
 const firebaseConfig = {
     apiKey: "AIzaSyCYTYtrsLipyXeWbOUR7sUm3NPLA0mHvgs",
     authDomain: "yerco-bb620.firebaseapp.com",
@@ -16,8 +10,5 @@ const firebaseConfig = {
     appId: "1:1035002416128:web:28ca04eb30b4dce8271ce5"
 };
 
-// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
-
-// Referencia a Firestore
 const db = firebase.firestore();
